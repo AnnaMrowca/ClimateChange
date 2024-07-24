5a1087d85964415cd85659546b70976ac6b5b607
AnnaMrowca <amrowca@gmail.com>
2024-07-24 21:03:12 +0200

    exrecises - apply

--- python_exercises.py
+++ python_exercises.py
@@ -5,6 +5,12 @@ pd.set_option("display.max_columns", 100)
 # Ćwiczenie stworzyć kolumnę, która będzie zawierała true jeśli w nazwie kraju będzie "U" - zastosuj funkcję apply
 # Cwiczenie poczytać o funkji apply i funkcji map
 
-input_path = "C:\\Users\\Ania\\Desktop\\ClimateChange\\processed_dataset.csv"
+input_path = "C:\\Users\\Ania\\Desktop\\ClimateChange\\GlobalLandTemperaturesByCountry.csv"
 df = pd.read_csv(input_path)
-print(df)
+
+letter_u = df['Country'].apply(lambda country: 'u' in country.lower())
+countries_with_u = df[letter_u]
+
+contains_93 = df['AverageTemperature'].apply(lambda temperature: '93' in str(temperature))
+temperatures_with_93 = df[contains_93]
+print(temperatures_with_93)
